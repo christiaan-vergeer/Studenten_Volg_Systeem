a6e88255ec868e80640ce748cd7246e7753cd0af
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2021-03-15 15:29:00 +0100

    moved unit testing to seperate project

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58471bbe-ac78-4d7a-93c7-b0de8236d411")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Testing.test
+{
+    [TestClass]
+    public class Studentshould
+    {
+        [TestMethod]
+        public void BeThere()
+        {
+            var Student = new Studenten_Volg_Systeem.Models.Student();
+            Student.Id = 0;
+            Assert.IsTrue(Student.Id == 0);
+        }
+    }
+
+}
+
+
+//[TestClass]
+//public class bucketshould
+//{
+//    [TestMethod]
+//    public void Beempty()
+//    {
+//        var bucket = new Bucket_OOP_Opdracht.bucket();
+//        Assert.IsTrue(bucket.Content == 0);
+//    }
+
+//    [TestMethod]
+//    public void BeDefaultCapacety()
+//    {
+//        var bucket = new Bucket_OOP_Opdracht.bucket();
+//        Assert.IsTrue(bucket.Capacety == 12);
+//    }
+
+//    [TestMethod]
+//    public void BeCustomCapacety()
+//    {
+//        var bucket = new Bucket_OOP_Opdracht.bucket(3);
+//        Assert.IsTrue(bucket.Capacety == 3);
+//    }
+
+//    [TestMethod]
+//    public void bePartiallyFilled()
+//    {
+//        var bucket = new Bucket_OOP_Opdracht.bucket();
+//        bucket.Fill(2);
+//        Assert.AreEqual(bucket.Content, 2);
+
+//    }
+
+//    [TestMethod]
+//    public void beFilledByOtherBucket()
+//    {
+//        var bucket1 = new Bucket_OOP_Opdracht.bucket();
+//        var bucket2 = new Bucket_OOP_Opdracht.bucket();
+//        bucket1.Fill(6);
+//        bucket2.Fill(bucket1);
+//        Assert.AreEqual(bucket2.Content, 6);
+//        Assert.AreEqual(bucket1.Content, 0);
+
+//    }
+
+//    [TestMethod]
+//    public void beAbleToOverflow()
+//    {
+//        bool trigger = false;
+//        var bucket = new Bucket_OOP_Opdracht.bucket();
+//        bucket.Fill(15);
+//        Assert.AreEqual(bucket.Content, bucket.Capacety);
+//        bucket.test += delegate ()
+//        {
+//            trigger = true;
+//        };
+//        Assert.AreEqual(bucket.Content, bucket.Capacety);
+//        Assert.IsTrue(trigger);
+//    }
+//}
+
+//[TestClass]
+//public class OilBarrelShould
+//{
+//    [TestMethod]
+//    public void beEmpty()
+//    {
+//        var barrel = new Bucket_OOP_Opdracht.models.oilbarrel();
+//        Assert.AreEqual(barrel.Content, 0);
+//    }
+
+//    [TestMethod]
+//    public void be159Liter()
+//    {
+//        var barrel = new Bucket_OOP_Opdracht.models.oilbarrel();
+//        Assert.AreEqual(barrel.Capacety, 159);
+//    }
+//}
+
+//[TestClass]
+//public class RainBucketShould
+//{
+//    [TestMethod]
+//    public void beEmpty()
+//    {
+//        var bucket = new Bucket_OOP_Opdracht.models.RainBucket("smalll");
+//        Assert.AreEqual(bucket.Content, 0);
+//    }
+
+//    [TestMethod]
+//    public void beSmall()
+//    {
+//        var bucket = new Bucket_OOP_Opdracht.models.RainBucket("small");
+//        Assert.AreEqual(bucket.Capacety, 80);
+//    }
+
+//    [TestMethod]
+//    public void beMedium()
+//    {
+//        var bucket = new Bucket_OOP_Opdracht.models.RainBucket("medium");
+//        Assert.AreEqual(bucket.Capacety, 120);
+//    }
+
+//    [TestMethod]
+//    public void beLarge()
+//    {
+//        var bucket = new Bucket_OOP_Opdracht.models.RainBucket("large");
+//        Assert.AreEqual(bucket.Capacety, 160);
+//    }
+//}
+
